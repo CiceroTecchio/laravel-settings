3cfdae3d8ce143323c2ca7567b2e676b42ca4f5c
CiceroTecchio <cicero_tecchio@hotmail.com>
2024-09-16 10:28:43 -0300

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,14 +1,14 @@
 <?php
 
 if (! function_exists('setting')) {
-    function setting($key = null, $default = null)
+    function setting($key = null, $default = null, $centralId = null)
     {
         $setting = app('setting');
 
         if (is_array($key)) {
             $setting->set($key);
         } elseif (! is_null($key)) {
-            return $setting->get($key, $default);
+            return $setting->get($key, $default, $centralId);
         }
 
         return $setting;
