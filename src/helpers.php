a323e2765110e46a5cf3d30d0653be927d023505
CiceroTecchio <cicero_tecchio@hotmail.com>
2024-09-16 18:25:31 -0300

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,21 +1,21 @@
 <?php
 
 if (! function_exists('setting')) {
-    // function setting($key = null, $default = null, $centralId = null)
-    // {
-    //     $setting = app('setting');
-
-    //     if (isset($centralId)){
-    //         $setting->centralId = $centralId;
-    //         $setting->load(true);
-    //     }
-
-    //     if (is_array($key)) {
-    //         $setting->set($key);
-    //     } elseif (! is_null($key)) {
-    //         return $setting->get($key, $default);
-    //     }
-
-    //     return $setting;
-    // }
+    function setting($key = null, $default = null, $centralId = null)
+    {
+        $setting = app('setting');
+    
+        if (isset($centralId) && $centralId != $setting->centralId) {
+            $setting->centralId = $centralId;
+            $setting->load(true);
+        }
+    
+        if (is_array($key)) {
+            $setting->set($key);
+        } elseif (! is_null($key)) {
+            return $setting->get($key, $default);
+        }
+    
+        return $setting;
+    }
 }
